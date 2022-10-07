d3c7e76019cc46f0437bf4c8eca45716de6ebf2a
unicorn03 <valentyna.pronina@nure.ua>
2022-10-07 12:26:16 +0200

    I have checked TableMultiplication method

--- src/Main.java
+++ src/Main.java
@@ -102,7 +102,7 @@ public class Main {
         for (int i = 1; i < 11; i++){
             for (int j = 1; j < 11; j++)
                 System.out.print(i*j+"\t");
-            System.out.println("\n");
+            System.out.print("\n");
         }
 
 
